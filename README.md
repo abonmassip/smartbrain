3f93aa23111413d4fe1cfa12391cac9532f6ab35
Albert <abonmassip@gmail.com>
2020-02-11 21:49:53 -0800

    image instruction

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 https://abonmassip-smartbrain.herokuapp.com/
 
 # Web app using Clarifai API to detect faces in images. 
-## Front-End made with React
-## Back-End server created in Node.js and Express.js
-## PostgreSQL database
-## Deployed in Heroku
+Front-End made with React, Back-End server created in Node.js and Express.js, PostgreSQL database and deployed in Heroku.
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} onKeyPress={(e) => {if(e.key === 'Enter'){onPictureSubmit()}}}/>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} onKeyPress={(e) => {if(e.key === 'Enter'){onPictureSubmit()}}} placeholder='paste image URL here'/>
                     <button 
                         className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                         onClick={onPictureSubmit}
